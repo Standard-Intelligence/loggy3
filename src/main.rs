b62ba1561c94d681b3c62be0fdf04a8d290f24df
Nicholas Charette <charettnr@gmail.com>
2025-02-25 16:25:52 -0800

    Better display fingerprint

--- src/main.rs
+++ src/main.rs
@@ -331,7 +331,7 @@ fn get_display_fingerprint() -> String {
         Err(_) => {
             let mut display_strings: Vec<String> = displays
                 .iter()
-                .map(|d| format!("{}:{}x{}, {}x{}", d.id, d.x, d.y, d.original_width, d.original_height))
+                .map(|d| format!("{}:{}x{} at x={},y={}", d.id, d.original_width, d.original_height, d.x, d.y))
                 .collect();
             display_strings.sort();
             display_strings.join(",")
